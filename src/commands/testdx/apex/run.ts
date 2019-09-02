a7a8e546ee9a19bf8520d91e37b8609cf59ecf88
Nikita Chursin <nchursin@aquivalabs.com>
2019-09-02 11:30:00 +0300

    --html flag to generate html in addition to default stdout

--- src/commands/testdx/apex/run.ts
+++ src/commands/testdx/apex/run.ts
@@ -14,9 +14,10 @@ const REPORT_NAME = 'report.html';
 
 const flagsConfig = ApexTestRunCommand.flagsConfig;
 flagsConfig.codecoverage = undefined;
-flagsConfig.resultformat = undefined;
+ApexTestRunCommand.resultFormatOptions.options.push('html');
 
-flagsConfig.coverage = flags.number({char: 'C', description: 'Code coverage threshold. The report will paint a class red in case coverage is less then this value. Defaults to 75'});
+flagsConfig.coverage = flags.number({char: 'C', description: 'Code coverage threshold. The report will paint a class red in HTML report in case coverage is less then this value. Defaults to 75'});
+flagsConfig.html = flags.boolean({description: 'Generates HTML report. Doesn\'t override --resultformat and --json flags, but works in addition'});
 
 export default class TestDXApexTestRunCommand extends ApexTestRunCommand {
 
@@ -26,18 +27,18 @@ export default class TestDXApexTestRunCommand extends ApexTestRunCommand {
 
   To run specific test classes, specify class names or suite names, or set a --testlevel value.
 
-  To run specific test methods, use --tests.
-
-  Generates html report.`;
+  To run specific test methods, use --tests.`;
 
   public static examples = [
-    '$ sfdx testdx:force:apex:run -n MyClassTest,MyOtherClassTest',
-    '$ sfdx testdx:force:apex:run -s MySuite,MyOtherSuite',
-    '$ sfdx testdx:force:apex:run -t MyClassTest.testCoolFeature,MyClassTest.testAwesomeFeature,AnotherClassTest,namespace.TheirClassTest.testThis',
-    '$ sfdx testdx:force:apex:run -l RunLocalTests -u me@my.org'
+    '$ sfdx testdx:apex:run -n MyClassTest,MyOtherClassTest',
+    '$ sfdx testdx:apex:run -s MySuite,MyOtherSuite',
+    '$ sfdx testdx:apex:run -t MyClassTest.testCoolFeature,MyClassTest.testAwesomeFeature,AnotherClassTest,namespace.TheirClassTest.testThis',
+    '$ sfdx testdx:apex:run -l RunLocalTests -u me@my.org'
   ];
 
   public static args = [{name: 'file'}];
+
+  public isToGenerateHtmlReport = false;
 
   public async run(): Promise<unknown> {
     const reportPath = this.getReportPath();
@@ -46,9 +47,10 @@ export default class TestDXApexTestRunCommand extends ApexTestRunCommand {
 
     const testRunResult = await super.run();
 
-    const html = parseToHtml(testRunResult as Report, this.flags['coverage']);
-
-    this.saveToFile(reportPath, html);
+    if (this.isToGenerateHtmlReport) {
+      const html = parseToHtml(testRunResult as Report, this.flags['coverage']);
+      this.saveToFile(reportPath, html);
+    }
 
     return testRunResult;
   }
@@ -69,22 +71,24 @@ export default class TestDXApexTestRunCommand extends ApexTestRunCommand {
   private normalizeFlags() {
     const coverage = this.flags['codecoverage'];
     const resultformat = this.flags['resultformat'];
+    const isResultFormatHtml = resultformat && resultformat === 'html';
+    this.isToGenerateHtmlReport = !!this.flags['html'] || isResultFormatHtml;
 
     // fill in defaults for ApexTestRunCommand to run properly
     if (!coverage) {
       this.flags['codecoverage'] = true;
     }
-    if (!resultformat) {
+    if (!resultformat || isResultFormatHtml) {
       this.flags['resultformat'] = 'json';
     }
   }
 
-  private saveToFile(path, html) {
-    fs.writeFile(path, html, err => {
+  private saveToFile(filePath, html) {
+    fs.writeFile(filePath, html, err => {
       if (err) {
           return console.error(err);
       }
-      console.log(`The report is generated: ${path}`);
+      console.log(`The report is generated: ${filePath}`);
     });
   }
 }
